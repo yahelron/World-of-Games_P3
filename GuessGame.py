203bf988fe71d88ce26763d4c3e1853bb1e5722e
yahelron <50333140+yahelron@users.noreply.github.com>
2020-01-09 17:29:13 +0200

    updated to Flask platform

--- GuessGame.py
+++ GuessGame.py
@@ -1,4 +1,31 @@
 import random
+from flask import Flask, jsonify, request
+# http://127.0.0.1:5001/parameters?level=3&guess=3
+app = Flask(__name__)
+secret_number_out = 0
+user_guess_num = 0
+@app.route('/parameters')
+def parameters():
+
+    level = int(request.args.get('level'))
+    guess = int(request.args.get('guess'))
+
+    global user_guess_num
+    user_guess_num = level
+    x = play(level)
+    x = str(x)
+    if guess > 5:
+        return jsonify(message= "numbers are too big."), 401
+    else:
+        return jsonify(level=level, guess = x, computer_generated_number = secret_number_out )
+
+
+@app.route('/url_variables/<string:level>/<int:guess>')
+def url_variables(level: int, guess: int):
+    if guess < 18:
+        return jsonify(message="Sorry " + level + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + level + ", you are old enough!")
 
 
 # Will get a number variable named difficulty
@@ -9,18 +36,20 @@ def generate_number(difficulty):
 
 
 # Will ask the user to guess a number between 1 to difficulty and return the number the user guessed.
-def get_guess_from_user(difficulty):
-    print("Guess a number between 1 to ", difficulty, end=' ')
-    user_guess_num = int(input())
+def get_guess_from_user(difficulty,user_guess_num):
+#    print("Guess a number between 1 to ", difficulty, end=' ')
+    print("your number is", user_guess_num)
     return user_guess_num
 
 
 # Will compare the secret generated number to the one prompted by the get_guess_from_user (number the user guessed)
 def compare_results(difficulty, secret_number):
-    if secret_number == get_guess_from_user(difficulty):
+    global secret_number_out
+    if secret_number == get_guess_from_user(difficulty,user_guess_num):
         print("Your Guessed number (", secret_number, ") is correct! ")
         return True
     else:
+        secret_number_out = secret_number
         print("Computer Generated number ", secret_number)
 
 
@@ -29,8 +58,12 @@ def play(difficulty):
     # Will return True / False if the user lost or won.
     x = compare_results(difficulty, generate_number(difficulty))
     if x is True:
-        return True
+        return "winner"
     else:
-        return False
+        return "loser"
 
+#user_guess_num = 3
+#print(play(5))
 
+if __name__ == '__main__':
+    app.run(port=5001)
---
+++ Main.py
@@ -0,0 +1,93 @@
+import math
+from flask import Flask, jsonify, request, render_template, redirect,url_for
+#from flask import Flask, render_template, request, redirect, url_for, flash
+import urllib.request, json
+from Score import add_score
+app = Flask(__name__)
+
+# http://127.0.0.1:5000/guessgame?myguess=19&name=myguess
+
+@app.route('/')
+def my_form():
+    return """<html>
+            <head>
+                <title>Scores Game</title>
+            </head>
+            <body>
+            <body>
+                <h1><div id="game" style="color:red">World of Games</div></h1>
+                <h3><div id="game2" style="color:blue">Hello and welcome to the World of Games(WoG). Here you can find many cool games to play</div></h3>
+                <h1><div id="link"> <a href="gamepicker">Start</a></div></h1>
+            </body>
+            </html>
+            """
+
+@app.route('/gamepicker')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/your-url', methods=['GET','POST'])
+def your_url():
+    user_game = request.form.get('game')
+    difficulty = request.form['level']
+    if user_game == 'memory':
+        return 'memory'
+    else:
+        return redirect(url_for('guessgame', myguess=6,name=difficulty))
+
+
+
+@app.route('/guessgame', methods=['GET','POST'])
+def guessgame():
+    try:
+        name = int(request.args.get('name'))
+        myguess = int(request.args.get('myguess'))
+        print('name=',name, "myguess=",myguess)
+        if request.method == 'POST':
+            myguess = request.values.get('guess_form')
+            myguess = int(myguess)
+            print("forom gusss is:",myguess)
+ #           handle_data = '/guessgame?myguess=%s&name=%d' % (myguess,name)
+            if math.isnan(myguess):
+                myguess = 6
+        if myguess < 7:
+            result = run_guess_game(myguess, name)
+            if result[1] == "winner":
+                points=add_score(name)
+                print("your points", points)
+                points=str(points)
+                return render_template('guess.html', difficulty=name, guess=myguess,result=result[1],points=points)
+            else:
+                return render_template('guess.html', difficulty=name, guess=myguess,result=result[1],points="No new points",win_num="The winner number was %d" % result[0])
+        else:
+            return "זבל"
+    except TypeError:
+        return render_template('guess.html', difficulty=5)
+    except ValueError:
+        return render_template('guess.html', difficulty=5)
+
+
+    # if myguess < 6:
+    #     return render_template('guess.html', difficulty=name, guess=myguess,result=run_guess_game(myguess,name))
+    #     #return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    # else:
+    #     return jsonify(message="you are not old enough!")
+
+# return render_template('guess.html', difficulty=difficulty, guess1="x=5")
+
+
+
+def run_guess_game(level,guess1):
+    url = urllib.request.urlopen(
+        "http://127.0.0.1:5001/parameters?level=%s&guess=%d" % (level,guess1))
+    data = json.loads(url.read().decode())  # Decoding a web request
+    # Parsing results
+    results = data['guess']
+    # return results
+    computer_generated_number = data['computer_generated_number']
+    return [computer_generated_number,results]
+
+
+if __name__ == '__main__':
+    app.run()
