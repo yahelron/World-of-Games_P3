9663035e6d4b027e486c4092f1f4e973ddb3de49
yahelron <50333140+yahelron@users.noreply.github.com>
2020-02-21 19:19:35 +0200

    Update e2e_guest.py

--- tests/e2e_guest.py
+++ tests/e2e_guest.py
@@ -4,10 +4,10 @@ import time
 ## check the guess game & the database
 ffdriver = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe",  executable_path='geckodriver.exe')
 ffdriver.get("http://3.124.186.74:5000/gamepicker")
-ffdriver.find_element_by_name("level").send_keys("4")
+ffdriver.find_element_by_name("level").send_keys("1")
 ffdriver.find_element_by_class_name("form-style-4").submit()
 time.sleep(2)
-ffdriver.find_element_by_name("guess_form").send_keys("5")
+ffdriver.find_element_by_name("guess_form").send_keys("1")
 ffdriver.find_element_by_class_name("form-style-4").submit()
 time.sleep(2)
 try:
