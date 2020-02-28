bda81091a514149b81d7cfa6e3cc34b50d20d5ef
yahelron <yahel.ron@gmail.com>
2020-02-29 01:11:21 +0200

    e2e_guest.py with click via cli

--- tests/e2e_guest.py
+++ tests/e2e_guest.py
@@ -1,24 +1,37 @@
+import click
 from selenium import webdriver
 import time
 
-## check the guess game & the database
-ffdriver = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe",  executable_path='geckodriver.exe')
-ffdriver.get("http://3.124.186.74:5000/gamepicker")
-ffdriver.find_element_by_name("level").send_keys("4")
-ffdriver.find_element_by_class_name("form-style-4").submit()
-time.sleep(2)
-ffdriver.find_element_by_name("guess_form").send_keys("5")
-ffdriver.find_element_by_class_name("form-style-4").submit()
-time.sleep(2)
-try:
-    x=ffdriver.find_element_by_id("result").text
-    print(x)
-    ffdriver.quit()
+#use like the following to run:  python file.py --ip http://3.84.16.226:5000/gamepicker
 
-except:
-    print("error")
-    ffdriver.quit()
-    exit(1)
 
-if x=="You are the winner!!!":
-    exit(0)
+@click.command()
+@click.option ('--ip',prompt="enter the ip add")
+
+def main(ip):
+    click.echo(f"your ip is {ip}")
+    ## check the guess game & the database
+    ffdriver = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe",  executable_path='geckodriver.exe')
+    ffdriver.get(ip)
+    ffdriver.find_element_by_name("level").send_keys("1")
+    ffdriver.find_element_by_class_name("form-style-4").submit()
+    time.sleep(2)
+    ffdriver.find_element_by_name("guess_form").send_keys("1")
+    ffdriver.find_element_by_class_name("form-style-4").submit()
+    time.sleep(2)
+    try:
+        x=ffdriver.find_element_by_id("result").text
+        print(x)
+        ffdriver.quit()
+
+    except:
+        print("error")
+        ffdriver.quit()
+        exit(1)
+
+    if x=="You are the winner!!!":
+        exit(0)
+
+
+if __name__=='__main__':
+    main()
