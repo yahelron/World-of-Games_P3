1710364df5b5c29d2266d0611dc0ab2ef5d9acc2
yahelron <yahel.ron@gmail.com>
2020-02-29 10:04:11 +0200

    e2e_guest, jenkins update

--- tests/e2e_guest.py
+++ tests/e2e_guest.py
@@ -2,17 +2,22 @@ import click
 from selenium import webdriver
 import time
 
+## check the guess game & the database
 #use like the following to run:  python file.py --ip http://3.84.16.226:5000/gamepicker
-
+ip = "192.168.1.30"
+url_suffix="gamepicker:5000"
+url = "http://%s/%s" % (ip,url_suffix)
+print(url)
 
 @click.command()
 @click.option ('--ip',prompt="enter the ip add")
 
 def main(ip):
     click.echo(f"your ip is {ip}")
-    ## check the guess game & the database
+    url_suffix = "gamepicker:5000"
+    url = "http://%s/%s" % (ip, url_suffix)
     ffdriver = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe",  executable_path='geckodriver.exe')
-    ffdriver.get(ip)
+    ffdriver.get(url)
     ffdriver.find_element_by_name("level").send_keys("1")
     ffdriver.find_element_by_class_name("form-style-4").submit()
     time.sleep(2)
